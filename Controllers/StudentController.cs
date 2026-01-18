f1b8c8ffd16c555e4bccd95fecff50417442d453
ducnt14112004 <ducnthe180869@fpt.edu.vn>
2026-01-18 13:29:35 +0700

    update layout

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ToanHocHay.WebApp.Controllers
+{
+    // [Authorize] // Bỏ comment dòng này nếu muốn bắt buộc đăng nhập mới vào được
+    public class StudentController : Controller
+    {
+        // URL: /Student/Dashboard
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        // URL: /Student/Profile
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
