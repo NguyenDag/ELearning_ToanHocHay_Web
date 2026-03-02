68bdb1da4f6a079b1de6eb15218b74ae1b9c7aa8
ducnt14112004 <ducnthe180869@fpt.edu.vn>
2026-03-03 01:42:16 +0700

    sửa code package

---
+++ Controllers/ParentController_New.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToanHocHay.WebApp.Controllers
+{
+    public class ParentController_New : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
